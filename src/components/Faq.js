5dc00eb8c76ee37c1d25c5fc0379e8cb04a4e8d0
Callum Lees <hi@c-l.dev>
2021-11-25 19:34:20 +0000

    Added content to FAQ and changed text weight in testimonial title

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -2,9 +2,24 @@ import React from "react";
 
 function Faq() {
   return (
-    <div className="flex bg-black min-h-screen mx-auto items-center justify-center text-center">
-      <div className="text-2xl text-white font-bold uppercase">FAQ</div>
-    </div>
+	  <div className="flex min-h-screen bg-gradient-to-r from-indigo-500 to-indigo-700">
+	  <div className="mx-auto text-center items-center text-2xl lg:text-4xl font-bold text-white grid place-items-center h-screen px-10">FAQ</div>
+	  	<div className="w-full mx-auto flex p-10 space-x-between gap-8 grid grid-cols-1 md:grid-cols-2">
+	  		<div className="">
+	  			<div className="ml-4 p-4 mt-10 mb-10 bg-white rounded-2xl">
+	  				<div className="text-2xl text-gray-700 font-bold">Q. Can I leave at any time?</div>
+	  				<div className="text-md mt-2 text-gray-500">A. Yes, we offer a contract as a "pay-as-you-go" - no strings attached!</div>	
+	  			</div>
+	  			<div className="ml-4 p-4 bg-white mb-10 rounded-2xl">
+	  				<div className="text-2xl text-gray-700 font-bold">Q. Is there a fixed term?</div>
+	  				<div className="text-md mt-2 text-gray-500">
+	  					A. No! We believe our loyal customers require free-will to be their creative best. We do not want to see you go but we value your time spent with us.
+	  				</div>
+	  			</div>	
+	  		</div>
+	  		<div className="bg-yellow-500"></div>
+	  	</div>
+	  </div>
   );
 }
 
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -10,7 +10,7 @@ const Testimonial = () => {
 </script>
 	return (
     <div className="flex flex-col justify-center min-h-screen mb-12 md:mb-0">
-      <div data-aos="fade-up" className="text-5xl font-thin text-black text-center mx-auto mb-12 mt-12 md:mt-0">
+      <div data-aos="fade-up" className="text-2xl lg:text-4xl font-bold text-gray-700 text-center mx-auto mb-12 mt-12 md:mt-0">
         Testimonials
       </div>
       <div data-aos="fade-up" className="flex grid grid-cols-1 md:grid-cols-2 mx-auto px-10 space-x-4 space-y-4">
