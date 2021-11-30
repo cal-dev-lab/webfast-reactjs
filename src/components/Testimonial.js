99c81290b57c2ea75ed09bb7bf6f1cfff618b26d
Callum Lees <hi@c-l.dev>
2021-11-30 15:54:15 +0000

    Removed testimonial card two

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -5,17 +5,20 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 
 const Testimonial = () => {
-<script>
-  AOS.init();
-</script>
-	return (
+  <script>AOS.init();</script>;
+  return (
     <div className="flex flex-col justify-center min-h-screen mb-12 md:mb-0">
-      <div data-aos="fade-up" className="text-2xl lg:text-4xl font-bold text-gray-700 text-center mx-auto mb-12 mt-12 md:mt-0">
+      <div
+        data-aos="fade-up"
+        className="text-2xl lg:text-4xl font-bold text-gray-700 text-center mx-auto mb-12 mt-12 md:mt-0"
+      >
         Testimonials
       </div>
-      <div data-aos="fade-up" className="flex grid grid-cols-1 md:grid-cols-2 mx-auto px-10 space-x-4 space-y-4">
+      <div
+        data-aos="fade-up"
+        className="flex grid grid-cols-1 mx-auto px-10 space-x-4 space-y-4"
+      >
         <TestimonialCardOne />
-        <TestimonialCardTwo />
       </div>
     </div>
   );
