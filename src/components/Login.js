46a7816fe5de10f011995249716653b04c82a7da
Callum Lees <hi@c-l.dev>
2022-01-16 23:45:50 +0000

    Updated colours on Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,7 +19,7 @@ const Login = (props) => {
       <div className=" p-10 rounded  w-full  max-w-lg">
         <div className="space-y-4">
           <div>
-            <div className="text-6xl text-center font-bold text-indigo-500">
+            <div className="text-6xl text-center text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-pink-500 font-light">
               webfast
             </div>
             <div className="text-center text-md font-thin mb-4">
@@ -27,23 +27,23 @@ const Login = (props) => {
             </div>
           </div>
           <hr className="mb-4" />
-          <label className="block mb-2 text-gray-800">Username</label>
+          <label className="block mb-2 text-gray-700">Username</label>
           <input
             type="text"
             placeholder="user@domain.com"
             autoFocus
             required
             value={email}
-            className="border-2 border-gray-200 w-full p-2 rounded outline-none focus:border-indigo-500"
+            className="border-2 border-gray-200 w-full p-2 rounded outline-none focus:border-pink-500"
             onChange={(e) => setEmail(e.target.value)}
           />
           <p>{emailError}</p>
-          <label className="block mb-2 text-gray-800">Password</label>
+          <label className="block mb-2 text-gray-700">Password</label>
           <input
             type="password"
             required
             value={password}
-            className="border-2 border-gray-200 w-full p-2 rounded outline-none focus:border-indigo-500"
+            className="border-2 border-gray-200 w-full p-2 rounded outline-none focus:border-pink-500"
             onChange={(e) => setPassword(e.target.value)}
           />
           <p>{passwordError}</p>
@@ -51,7 +51,7 @@ const Login = (props) => {
             {hasAccount ? (
               <>
                 <button
-                  className="block bg-indigo-500 hover:bg-indigo-600 transition text-white duration-300 w-full p-2 rounded"
+                  className="block bg-gradient-to-tr from-red-500 to-pink-500 transition text-white duration-300 w-full p-2 rounded"
                   onClick={handleLogin}
                 >
                   Sign in
@@ -59,7 +59,7 @@ const Login = (props) => {
                 <p className="text-gray-700 text-sm mt-4">
                   Don't have an account?
                   <span
-                    className="text-sm text-indigo-500 ml-2"
+                    className="text-sm text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-pink-500 ml-2"
                     onClick={() => setHasAccount(!hasAccount)}
                   >
                     Sign up
@@ -69,7 +69,7 @@ const Login = (props) => {
             ) : (
               <>
                 <button
-                  className="block bg-indigo-500 hover:bg-indigo-600 transition text-white duration-300 w-full p-2 rounded"
+                  className="block bg-gradient-to-tr from-red-500 to-pink-500 transition text-white duration-300 w-full p-2 rounded"
                   onClick={handleSignUp}
                 >
                   Sign up
@@ -77,7 +77,7 @@ const Login = (props) => {
                 <p className="text-gray-700 text-sm mt-4">
                   Have an account?{" "}
                   <span
-                    className="text-sm text-indigo-500 ml-2"
+                    className="text-sm text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-pink-500 ml-2"
                     onClick={() => setHasAccount(!hasAccount)}
                   >
                     Sign in
