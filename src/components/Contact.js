ced5214e65a2d3d109be77901d4044e23f993085
Callum Lees <hi@c-l.dev>
2022-01-16 23:43:35 +0000

    Added content for Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,20 @@
 import React from "react";
 
-function Contact() {
+const Contact = () => {
   return (
-    <div className="flex bg-indigo-500 min-h-screen mx-auto items-center justify-center text-center">
-      <div className="text-2xl text-white font-bold uppercase">Contact</div>
+    <div className="w-full relative">
+      <div className="w-full h-full top-0 left-0 absolute"></div>
+      <div className="w-full h-full flex flex-col-reverse absolute top-0 left-0 justify-center mx-auto items-center">
+        <a href="/#">
+          <button className="text-xs md:text-md py-2 px-3 rounded-md font-bold tracking-widest text-white uppercase mt-10 hover:text-pink-500 hover:bg-white transition duration-300">
+            Contact us &rarr;
+          </button>
+        </a>
+        <div className="text-7xl md:text-9xl font-thin text-white">Get in touch!</div>
+      </div>
+      <video loop autoPlay muted className="h-96 w-full object-cover">
+        <source src={require('../media/technology-contact.mp4')} type="video/mp4" />
+      </video>
     </div>
   );
 }
