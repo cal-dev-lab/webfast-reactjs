e31722522e79e2cffa63b52d338a11353828a06c
Callum Lees <hi@c-l.dev>
2022-01-16 23:42:42 +0000

    Updated font and grid col setup on CardGallery.js

--- src/components/CardGallery.js
+++ src/components/CardGallery.js
@@ -6,18 +6,18 @@ import CardThree from "./CardThree";
 const CardGallery = () => {
   return (
     <div>
-      <div className="w-full h-full p-10">
+      <div className="w-full h-full p-10 font-light">
         {/* Heading */}
-        <div className="text-2xl lg:text-4xl font-light text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-pink-500 text-center items-center justify-center mb-6">
+        <div className="text-4xl font-light text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-pink-500 text-center items-center justify-center mb-6">
           Hosting Plans
         </div>
-        <div className="text-md text-gray-500 text-center items-center justify-center px-12">
+        <div className="text-sm md:text-lg text-gray-700 text-center items-center justify-center">
           We offer the fastest Linux servers fully equipped with your own
           dedicated memory and storage.
         </div>
       </div>
       {/* Three cards */}
-      <div className="max-w-7xl mx-auto grid grid-cols-1 xl:grid-cols-3 mt-6">
+      <div className="max-w-7xl p-10 gap-20 mx-auto grid grid-cols-1 xl:grid-cols-3">
         <CardOne />
         <CardTwo />
         <CardThree />
