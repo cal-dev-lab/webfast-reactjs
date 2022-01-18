f442ab9ed8e82f227de401b089b927d9b3650f76
Callum Lees <hi@c-l.dev>
2022-01-18 22:43:15 +0000

    Created LearnMore.js

--- src/components/LearnMore.js
+++ src/components/LearnMore.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function LearnMore() {
+    useEffect(() => {
+        Aos.init({ duration: 2000 });
+    }, []);
     return (
-        <div class="bg-gray-50">
-            <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+        <div class="bg-gray-50" data-aos="fade-up">
+            <div class="max-w-7xl mx-auto py-12 p-10 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
                 <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                     <span class="block">Want to experience the way forward?</span>
                     <span class="block text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-pink-500">Try us out for 30 days free.</span>
