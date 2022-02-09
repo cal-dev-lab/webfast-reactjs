a8536e3cb20369f4c5d4c8cd11975e0de91f069e
Callum Lees <hi@c-l.dev>
2022-02-09 22:03:53 +0000

    Removed static bar in nav on Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,26 +10,6 @@ const Navbar = ({ handleLogout }) => {
   return (
   <>
     <nav className="bg-white sticky top-0 z-30">
-      {/* START: Phone bar */}
-      <div className="px-8 py-1 bg-gradient-to-tr from-red-500 to-pink-500 mx-auto sticky top-0">
-        <div className="max-w-6xl mx-auto">
-          <div className="flex justify-between">
-            <div className="flex">
-              <div className="flex items-center space-x-1">
-                <svg className="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path></svg>
-                <p
-                  className="text-white"
-                >
-                  <a href="tel:0121300003">
-                    0121 300 003
-                  </a>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* END: Phone bar */}
       {/* START: Main Navbar */}
       <div className="max-w-6xl px-8 mx-auto">
         <div className="flex justify-between">
